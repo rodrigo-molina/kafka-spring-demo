57dac7914212826d894ab69a3bc1b79d802b2f0c
rodrigo-molina <43792562+rodrigo-molina@users.noreply.github.com>
2019-10-15 18:19:49 +0200

    Add Spring boot initilializer

---
+++ src/main/java/com/example/spring/kafka/KafkaSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringExampleApplication.class, args);
+	}
+
+}
